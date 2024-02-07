229a48fc10d61f1219e1bc1ae31f1b4d179da1e7
dpdahal <dp48dahal@gmail.com>
2024-02-07 14:43:59 +0545

    update

---
+++ datatypes/main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+    int phone = 984336;
+    printf("Size of int: %d\n", phone);
+
+}
+
+/*
+
+## What is data type?
+* Data type is a classification of data, which tells the 
+compiler or interpreter how the programmer intends to use the data.
+
+### List of data types:
+* int: %d, %i - 4 bytes
+* float: %f - 4 bytes
+* double: %lf - 8 bytes
+* char: %c - 1 byte
+* void: void - 1 byte
+* short: %hd
+* long: %ld
+* long long: %lld
+* signed: signed
+* unsigned: unsigned
+* struct, union, enum: struct, union, enum
+* array: array
+* pointer: & 
+
+*/
